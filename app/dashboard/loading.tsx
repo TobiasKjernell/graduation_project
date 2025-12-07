033c258a3bdd2b441343b126df029e4447f95bb7
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-12-07 19:16:10 +0100

    added loading fallback for dashboards

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -2,7 +2,10 @@ import { Spinner } from "@/components/ui/spinner";
 
 const Loading = () => {
     return (
-        <Spinner className="size-96" />   
+        <div className="h-screen flex items-center justify-center">
+              <Spinner className="size-50" />  
+        </div>
+       
     )
 }       
 export default Loading; 
--- components/LevelsGrid/index.tsx
+++ components/LevelsGrid/index.tsx
@@ -8,7 +8,7 @@ const LevelsGrid = async ({ project }: { project: string }) => {
    
     const { data, error } = await getLevelsByProject(getProjectNameByString(project))
     if (error) return <div>Something went wrong: {error.message}</div>
-
+    
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4">
             {data && data.map(item => <LevelGridItem key={item.id} {...item} project_id={getProjectNameByString(project)} />)}
