61bd2fad37da417cb72f4ec83227262b609457af
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-12-07 20:02:33 +0100

    cachelife test version 2

--- app/dashboard/[project]/levels/page.tsx
+++ app/dashboard/[project]/levels/page.tsx
@@ -1,9 +1,12 @@
 import DashboardBreadcrumb from "@/components/DashboardBreadcrumb";
 import LandingSpinner from "@/components/LandingSpinner";
 import LevelsGrid from "@/components/LevelsGrid";
+import { cacheLife } from "next/cache";
 import { Suspense } from "react";
 
 const LevelsPage = async ({ params }: { params: Promise<{ project: string }> }) => {
+     'use cache'
+   cacheLife('default')
     const { project } = await params;
     return (
         <div>
--- components/LevelsGrid/index.tsx
+++ components/LevelsGrid/index.tsx
@@ -1,11 +1,9 @@
 import { getLevelsByProject } from "@/lib/supabase/queries"
 import { getProjectNameByString } from "@/lib/utils";
 import LevelGridItem from "../LevelGridItem";
-import { cacheLife } from "next/cache";
 
 const LevelsGrid = async ({ project }: { project: string }) => {
-   'use cache'
-   cacheLife('default')
+  
     const { data, error } = await getLevelsByProject(getProjectNameByString(project))
     if (error) return <div>Something went wrong: {error.message}</div>
 
